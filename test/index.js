917f4c7e10a43fa78cf636fe19a8f8b4c87fbc49
blugavere <b.lugavere@gmail.com>
2017-07-01 11:21:04 -0400

    wait server done?

--- test/index.js
+++ test/index.js
@@ -14,7 +14,7 @@ describe('middlewares', () => {
     next();
   };
   const sendLocals = (req, res) => res.send(res.locals);
-  before(() => {
+  before(done => {
     app = express();
     middlewares = new MiddlewareContainer();
     middlewares.register('getApiVersion', (req, res, next) => assignNext({
@@ -37,11 +37,13 @@ describe('middlewares', () => {
     app.get('/v', middlewares.get('getApiVersion'), sendLocals);
     app.get('/foo', middlewares.get('getFoo'), sendLocals);
     app.get('/', sendLocals);
-    server = app.listen(3000);
+    server = app.listen(3000, done);
   });
+
   after(() => {
     server.close();
   });
+
   it('should export something', () => {
     expect(middlewares).toExist();
   });
@@ -50,12 +52,14 @@ describe('middlewares', () => {
     return request.get('http://localhost:3000').send()
       .then(res => expect(res.body).toEqual({}));
   });
+
   it('singular case', () => {
     return request.get('http://localhost:3000/v').send()
       .then(res => expect(res.body).toEqual({
         v: 1
       }));
   });
+
   it('dependent case', () => {
     return request.get('http://localhost:3000/foo').send()
       .then(res => expect(res.body).toEqual({
