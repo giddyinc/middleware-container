d5a7b27962d438de2bd9488bcf819ccb40f1d93e
Ben Lugavere <b.lugavere@gmail.com>
2020-06-03 12:08:11 -0700

    parallelize leaf nodes by default

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import { Injector } from 'boxed-injector';
 import connect from 'connect';
+import parallel from 'async.parallel';
 
 const handler: ProxyHandler<Injector> = {
   get(target, propKey) {
@@ -18,12 +19,44 @@ const handler: ProxyHandler<Injector> = {
       const dep: string | string[] = args[0];
 
       // get the dependency graph
-      return target.graph(dep)
+      const graph = target.graph(dep);
+      // console.log(graph);
+
+      const [ parallelizable, sequence ] = graph
+        .reduce((maps, middleware) => {
+          const [ parallelizable, sequence ] = maps;
+          // @ts-ignore
+          const dependencies = target.instances?.[middleware]?.depends ?? [];
+          if(dependencies.length > 0) {
+            sequence.push(middleware);
+          } else {
+            parallelizable.push(middleware);
+          }
+          return maps;
+        }, [[], []] as Readonly<[string[], string[]]>);
+
+      const combinedParallelizable = (req, res, next) => {
+        const wares = parallelizable
+          .map(mw => target.get(mw))
+          .map(mw => next => mw(req, res, next));
+        return parallel(wares, next);
+      };
+
+      const middleware = connect();
+
+      if(parallelizable.length) {
+        // console.log('paralellizing middlewares: ', parallelizable);
+        middleware.use(combinedParallelizable);
+      }
+
+      // console.log('sequencing middlewares: ', sequence);
+
+      return sequence
         // create a chained connect which combines all of the middlewares
         .reduce((chain, middleware) => {
           chain.use(target.get(middleware));
           return chain;
-        }, connect());
+        }, middleware);
     };
   }
 };
