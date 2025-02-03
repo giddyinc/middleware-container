ad831ff580daff97906224447e643bf5efb5d647
Holden McGinnis <holdenjmcginnis@gmail.com>
2025-02-03 11:33:09 -0500

    revert parallelization due to breaking errors in upstream test suites (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { Injector } from "boxed-injector";
 import connect from "connect";
-import parallel from "async.parallel";
-import { RequestHandler } from "express";
 
 const handler: ProxyHandler<Injector> = {
   get(target, propKey) {
@@ -19,48 +17,14 @@ const handler: ProxyHandler<Injector> = {
       const dep: string | string[] = args[0];
 
       // get the dependency graph
-      const graph = target.graph(dep);
-      // console.log(graph);
-
-      const [parallelizable, sequential] = graph.reduce(
-        (groups, middleware) => {
-          const [parallelizable, sequential] = groups;
-          // @ts-ignore
-          const dependencies = target.instances?.[middleware]?.depends ?? [];
-          if (dependencies.length > 0) {
-            sequential.push(middleware);
-          } else {
-            parallelizable.push(middleware);
-          }
-          return groups;
-        },
-        [[], []] as Readonly<[string[], string[]]>
-      );
-
-      const parallelMiddleware = parallelizable.map((mw) => target.get(mw));
-      const combinedParallelizable: RequestHandler = (req, res, next) => {
-        const asyncFns = parallelMiddleware.map(
-          (mw) => (next) => mw(req, res, next)
-        );
-        return parallel(asyncFns, next);
-      };
-
-      const middleware = connect();
-
-      if (parallelizable.length) {
-        // console.log('paralellizing middlewares: ', parallelizable);
-        middleware.use(combinedParallelizable);
-      }
-
-      // console.log('sequencing middlewares: ', sequential);
-
       return (
-        sequential
+        target
+          .graph(dep)
           // create a chained connect which combines all of the middlewares
           .reduce((chain, middleware) => {
             chain.use(target.get(middleware));
             return chain;
-          }, middleware)
+          }, connect())
       );
     };
   },
